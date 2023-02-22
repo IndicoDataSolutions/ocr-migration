c066c570d0f568c5781d71a3645b3c656b36e8e9
Christopher M. Wells <73897499+chris-wells-13@users.noreply.github.com>
2023-02-22 15:24:34 -0500

    Add files via upload

---
+++ comparison_helpers.py
@@ -0,0 +1,136 @@
+from indico_toolkit.metrics.compare_ground_truth import CompareGroundTruth
+from itertools import groupby
+import json
+import pandas as pd
+
+
+def convert_results(res):  # sourcery skip: extract-duplicate-method
+    all_spans = [r["spans"] for r in res]
+    all_spans = [dict(item, **{"checked": False}) for item in all_spans]
+    all_converted = []
+    for a in all_spans:
+        if a["checked"]:
+            continue
+        converted = {
+            "label": a["label"],
+            "text": a["text"],
+            "start": a["text_spans"][0]["start"],
+            "end": a["text_spans"][0]["end"],
+        }
+        adjacent = True
+        last_end = a["text_spans"][0]["start"] - 1
+        next_start = a["text_spans"][0]["end"] + 1
+        while adjacent:
+            left_adjacent = [
+                s
+                for s in all_spans
+                if s["text_spans"][0]["end"] == last_end and s["label"] == a["label"]
+            ]
+            right_adjacent = [
+                s
+                for s in all_spans
+                if s["text_spans"][0]["start"] == next_start
+                and s["label"] == a["label"]
+            ]
+            if left_adjacent:
+                converted["text"] = left_adjacent[0]["text"] + " " + converted["text"]
+                converted["start"] = left_adjacent[0]["text_spans"][0]["start"]
+                a["checked"] = True
+                left_adjacent[0]["checked"] = True
+            if right_adjacent:
+                converted["text"] += " " + right_adjacent[0]["text"]
+                converted["end"] = right_adjacent[0]["text_spans"][0]["end"]
+                a["checked"] = True
+                right_adjacent[0]["checked"] = True
+
+            adjacent = left_adjacent or right_adjacent
+            last_end = converted["start"] - 1
+            next_start = converted["end"] + 1
+        all_converted.append(converted)
+
+    return all_converted
+
+
+def convert_for_file(labels, results_for_file):
+    # sourcery skip: inline-immediately-returned-variable
+    all_converted = []
+    for page_num, res in results_for_file.items():
+        all_converted.extend(convert_results(res))
+
+    gt_compare = CompareGroundTruth(ground_truth=labels, predictions=all_converted)
+    gt_compare.set_all_label_metrics("overlap")
+    gt_compare.set_overall_metrics()
+    final = {
+        "overall": gt_compare.overall_metrics,
+        "by_label": gt_compare.all_label_metrics,
+    }
+    return final
+
+
+def collapse(preds):
+    collapsed = []
+    for index, grouped_preds in groupby(
+        preds, key=lambda x: x["token"]["prediction_index"]
+    ):
+        to_list = list(grouped_preds)
+        to_list.sort(key=lambda x: x["spans"]["text_spans"][0]["start"])
+        collapsed.append(
+            {
+                "text": " ".join([t["spans"]["text"] for t in to_list]),
+                "start": to_list[0]["spans"]["text_spans"][0]["start"],
+                "end": to_list[-1]["spans"]["text_spans"][0]["end"],
+                "label": to_list[0]["spans"]["label"],
+                "page_num": to_list[0]["spans"]["page_num"],
+            }
+        )
+    return collapsed
+
+
+def filter_empty(preds):
+    return filter(lambda x: x["spans"]["text_spans"], preds)
+
+
+def summarize_results(results, labels_raw):
+    records_by_file = {}
+    for file_name in results.keys():
+        all_transformed = results[file_name]
+        flattened = []
+        for _, a_t in all_transformed.items():
+            flattened.extend(collapse(filter_empty(list(a_t))))
+        original_labels = json.loads(
+            labels_raw[labels_raw["file_name"] == file_name]["labels"].values[0]
+        )
+        missing_records = []
+        matched_records = []
+        # flatten labels and page number
+        for o in original_labels:
+            label = o["label"]
+            text = o["text"]
+            if matches := [
+                t for t in flattened if t["label"] == label and t["text"] == text
+            ]:
+                matched_records.append(
+                    {
+                        "original_label": o,
+                        "page_number": matches[0]["page_num"],
+                        "new_span": matches,
+                    }
+                )
+            else:
+                missing_records.append({"original_label": o})
+        records_by_file[file_name] = {
+            "matched": matched_records,
+            "missing": missing_records,
+        }
+
+    return records_by_file
+
+
+def convert_to_excel(results_dict, out_file):
+    writer = pd.ExcelWriter(out_file, engine="xlsxwriter")
+    for file_name, results in results_dict.items():
+        df = pd.DataFrame.from_records(
+            [r["original_label"] for r in results["missing"]], index=None
+        )
+        df.to_excel(writer, sheet_name=file_name[:30], index=None)
+    writer.close()
---
+++ ocr_migration.py
@@ -0,0 +1,427 @@
+import numpy as np
+import imutils
+import cv2
+import pandas as pd
+import json
+import pandas as pd
+import json
+import string
+import itertools
+from indico_toolkit.association import ExtractedTokens
+from copy import copy
+import logging
+import yaml
+from fire import Fire
+
+logging.basicConfig(
+    filename="ocr_migration.log",
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+)
+
+from geometry_helpers import (
+    AlignerConfig,
+    normalize_position,
+    normalize_tokens,
+    in_box,
+    get_word_match_dict,
+    transform_tokens,
+    pixel_distance,
+)
+
+from comparison_helpers import summarize_results, convert_to_excel
+
+
+def get_match_words_from_page(page_ocr, granularity="tokens"):
+    # sourcery skip: de-morgan, inline-immediately-returned-variable
+    tokens = [t["text"] for t in page_ocr[granularity]]
+    filtered = [w for w in tokens if not w.lower() in string.punctuation]
+    return filtered
+
+
+def get_keypoints(
+    image_matches, template_matches, image_size, template_size, debug=False
+):
+    """
+    Given a set of words that occur in both the image and the candidate template,
+    creates lists of cv2.DMatch and cv2.KeyPoint instances which can be used to
+    construct a homography matrix
+    Parameters:
+        image_matches (dict): maps word occurring in both docs to list of
+                              image tokens containing that word
+        template_matches (dict): Same as above but with template
+        image_size (dict): size in pixels with keys 'height', 'width'
+        template_size (dict): size in pixels with keys 'height', 'width'
+    Returns:
+    kps: (KeyPoint list, KeyPoint list) of image and template keypoint lists
+    matches: (DMatch list): list of created DMatch instances
+    """
+    kpsA = []
+    kpsB = []
+    matches = []
+
+    # Sorting words because we later sort matches by distance, and that
+    # order is not deterministic if there are multiple matches with
+    # identical distances, which can happen if the difference between
+    # top/bottom and left/right is identical for two matches
+    for word in sorted(image_matches.keys()):
+        ims = image_matches[word]
+        temps = template_matches[word]
+
+        # Product between every token in image and every token in template
+        # that has the same word
+        for im, temp in itertools.product(ims, temps):
+            cv_match = cv2.DMatch(
+                _distance=pixel_distance(im["nposition"], temp["nposition"]),
+                _queryIdx=len(matches),
+                _trainIdx=len(matches),
+                _imgIdx=0,
+            )
+
+            matches.append(cv_match)
+
+            # Keypoints on image
+            kp_im = cv2.KeyPoint(
+                x=image_size["width"]
+                * (im["nposition"]["left"] + im["nposition"]["right"])
+                / 2,
+                y=image_size["height"]
+                * (im["nposition"]["bottom"] + im["nposition"]["top"])
+                / 2,
+                size=1,
+            )
+            kpsA.append(kp_im)
+
+            # Keypoints on template
+            kp_temp = cv2.KeyPoint(
+                x=template_size["width"]
+                * (temp["nposition"]["left"] + temp["nposition"]["right"])
+                / 2,
+                y=template_size["height"]
+                * (temp["nposition"]["bottom"] + temp["nposition"]["top"])
+                / 2,
+                size=1,
+            )
+            kpsB.append(kp_temp)
+
+    return (kpsA, kpsB), matches
+
+
+def keypoints(
+    *,
+    config,
+    matches,
+    kps,
+    template_image=None,
+    image=None,
+    debug=False,
+):
+    """Analyze images with ORB and RANSAC.
+    Parameters:
+        config (dict): config parameters
+        template_image: cv2 image object to be analyzed
+        template: cv2 image object to be analyzed
+        keepFraction (float) : how many matches to keep for alignment
+        scale_factor (float) : scale image after alignment
+        min_distance (float) : maximum distance (0-1 scale) to keep for alignment
+        debug (bool) : if True display keypoints for manual check before alignment
+        matches (cv2.DMatch list) : list of matches between keypoints to use
+        kps (cv2.KeyPoint list) : list of keypoints to use
+    Returns:
+        Aligned image object.
+    """
+    kpsA, kpsB = kps
+
+    # sort the matches by their distance (the smaller the distance, the
+    # "more similar" the features are). Need to also sort by the queryIdx
+    # so that the order is deterministic in case there are multiple
+    # matches with the same distance
+    matches = [m for m in matches if m.distance <= config.MAX_PIXEL_DISTANCE]
+    matches = sorted(matches, key=lambda x: (x.distance, x.queryIdx))
+    # keep only the top matches
+    matches = matches[: config.NUM_KEYPOINTS_FOR_ALIGNMENT]
+
+    if len(matches) < 5:
+        return None, None
+
+    # visualize the matched keypoints
+    if debug:
+        matchedVis = cv2.drawMatches(image, kpsA, template_image, kpsB, matches, None)
+        matchedVis = imutils.resize(matchedVis, width=1300)
+        cv2.imshow("Matched Keypoints", matchedVis)
+        cv2.waitKey(0)
+
+    # allocate memory for the keypoints (x,y-coordinates) from the # top matches
+    # we'll use these coordinates to compute our homography matrix
+    ptsA = np.zeros((len(matches), 2), dtype="float")
+    ptsB = np.zeros((len(matches), 2), dtype="float")
+    # loop over the top matches
+    for i, m in enumerate(matches):
+        # indicate that the two keypoints in the respective images map to each other
+        ptsA[i] = kpsA[m.queryIdx].pt
+        ptsB[i] = kpsB[m.trainIdx].pt
+
+    # compute the homography matrix between the two sets of matched points
+    affine_method = (
+        cv2.estimateAffinePartial2D if config.PARTIAL_AFFINE else cv2.estimateAffine2D
+    )
+    (H, mask) = affine_method(
+        ptsA,
+        ptsB,
+        method=cv2.RANSAC,
+        ransacReprojThreshold=config.ransacReprojThreshold,
+        maxIters=config.maxIters,
+        refineIters=config.refineIters,
+        confidence=config.confidence,
+    )
+
+    if debug:
+        successful_matches = [
+            match for mask_val, match in zip(mask, matches) if mask_val
+        ]
+        matchedVis = cv2.drawMatches(
+            image, kpsA, template_image, kpsB, successful_matches, None
+        )
+        matchedVis = imutils.resize(matchedVis, width=1300)
+        cv2.imshow("Matched Keypoints", matchedVis)
+        cv2.waitKey(0)
+
+    return H, mask
+
+
+def extract_text_spans_from_box(
+    config, template_width, template_height, image_tokens, box
+):
+    """
+    Given a box on a template, extract image tokens that lie within the
+    box.
+    Parameters:
+        template_width (int): prediction dict returned from model.predict()
+        template_height (int): offset dict return from Aligner.acord_alignment_fix
+        image_tokens (dict list): list of tokens on input image, as returned
+            from OCR
+        box (dict list): list of boxes on template - currently as returned
+            from OmniPage template constructor
+    """
+    normalized_box = normalize_position(
+        box["position"], template_width, template_height
+    )
+    # TODO: Fix this O(N^2) operation with something like a KD-Tree
+    toks_in_box = list(
+        filter(
+            lambda tok: in_box(
+                tok["nposition"],
+                normalized_box,
+                atol=config.TEXT_EXTRACTION_ATOL,
+            ),
+            image_tokens,
+        )
+    )
+    toks = sorted(toks_in_box, key=lambda t: t["position"]["top"])
+    text_spans, text = order_text_spans_from_zone(config, box, toks)
+
+    return {
+        "label": box["label"],
+        "text_spans": text_spans,
+        "text": text,
+        "page_num": box["page_num"],
+    }
+
+
+def order_text_spans_from_zone(config, box, toks):
+    """
+    Given the extracted tokens from a box, orders tokens by position and
+    returns the GlobalTokenSpan information (start, end, page_num)
+    for each piece of text, as well as the joined text string
+    """
+    if len(toks) == 0:
+        return [], ""
+    line_breaks = [0] + [
+        i
+        for i in range(1, len(toks))
+        if toks[i]["position"]["top"] - toks[i - 1]["position"]["top"]
+        > config.NEWLINE_PIXELS_THRESHOLD
+    ]
+
+    # split tokens into separate lines
+    ordered_tokens = []
+    if len(line_breaks) == 1:
+        ordered_tokens.append(sorted(toks, key=lambda t: t["position"]["left"]))
+    else:
+        for idx1, idx2 in zip(line_breaks, line_breaks[1:]):
+            line = toks[idx1:idx2]
+            ordered_tokens.append(sorted(line, key=lambda t: t["position"]["left"]))
+        last_line = toks[line_breaks[-1] :]
+        ordered_tokens.append(sorted(last_line, key=lambda t: t["position"]["left"]))
+    text = "\n".join(" ".join(tok["text"] for tok in line) for line in ordered_tokens)
+
+    # Return start/end indices for each group of tokens. If tokens are
+    # contiguous, join them
+    text_spans = []
+    for line in ordered_tokens:
+        prev_end = None
+        for token in line:
+            if prev_end is not None and token["page_offset"]["start"] - 1 == prev_end:
+                # Join with previous token rather than creating new text span
+                text_spans[-1]["end"] = token["page_offset"]["end"]
+            else:
+                text_spans.append(
+                    {
+                        "start": token["page_offset"]["start"],
+                        "end": token["page_offset"]["end"],
+                        "page_num": box["page_num"],
+                    }
+                )
+            prev_end = token["page_offset"]["end"]
+
+    return text_spans, text
+
+
+def run_all_pages_for_doc(
+    file,
+    new_ocr_for_file,
+    old_ocr_for_file,
+    aligner_config,
+    debug=False,
+):
+    new_images = json.loads(new_ocr_for_file["image_files"].values[0])
+    new_ocr = json.loads(new_ocr_for_file["ocr"].values[0])
+
+    old_ocr = json.loads(old_ocr_for_file["ocr"].values[0])
+    old_images = json.loads(old_ocr_for_file["image_files"].values[0])
+    labels = json.loads(old_ocr_for_file["labels"].values[0])
+
+    label_to_token_by_page = {}
+    for p_0, n_0 in zip(old_ocr, new_ocr):
+        old_page_ocr = p_0["pages"][0]
+        page_number = old_page_ocr["page_num"]
+        min_offset = old_page_ocr["doc_offset"]["start"]
+        max_offset = old_page_ocr["doc_offset"]["end"]
+
+        old_size = old_page_ocr["size"]
+        old_tokens = p_0["tokens"]
+        norm_old_tokens = normalize_tokens(old_tokens, old_size)
+
+        new_page_ocr = n_0["pages"][0]
+        new_size = new_page_ocr["size"]
+        new_tokens = n_0["tokens"]
+        norm_new_tokens = normalize_tokens(new_tokens, new_size)
+
+        if aligner_config.granularity == "tokens":
+            norm_old_points = norm_old_tokens
+            norm_new_points = norm_new_tokens
+
+        else:
+            norm_old_points = normalize_tokens(p_0["chars"], old_size)
+            norm_new_points = normalize_tokens(n_0["chars"], new_size)
+
+        matches = get_match_words_from_page(p_0, aligner_config.granularity)
+        new_match, old_match = get_word_match_dict(
+            norm_new_points, norm_old_points, matches
+        )
+
+        kps, cv_matches = get_keypoints(old_match, new_match, old_size, new_size)
+
+        original_image = cv2.imread(old_images[page_number])
+        new_image = cv2.imread(new_images[page_number])
+        homography, mask = keypoints(
+            config=aligner_config,
+            kps=kps,
+            matches=cv_matches,
+            template_image=new_image,
+            image=original_image,
+            debug=debug,
+        )
+        if homography is None:
+            message = f"RANSAC failed for {file} on page {page_number}, using identity matrix."
+            print(message)
+            logging.warning(message)
+            affine_warp = np.identity(3)
+        else:
+            affine_warp = np.vstack([homography, [0, 0, 1]])
+
+        ex_tokens, transformed = transform_and_extract(
+            labels, min_offset, max_offset, old_tokens, affine_warp
+        )
+
+        label_token_map = []
+        for t, o in zip(transformed, ex_tokens.extracted_tokens):
+            extracted_spans_for_label = extract_text_spans_from_box(
+                aligner_config,
+                new_size["width"],
+                new_size["height"],
+                norm_new_tokens,
+                t,
+            )
+            label_token_map.append(
+                {"token": t, "spans": extracted_spans_for_label, "original_token": o}
+            )
+
+        label_to_token_by_page[page_number] = label_token_map
+
+    return label_to_token_by_page
+
+
+def transform_and_extract(labels, min_offset, max_offset, old_tokens, affine_warp):
+    labels_on_page = [
+        l for l in labels if l["end"] <= max_offset and l["start"] >= min_offset
+    ]
+    ex_tokens = ExtractedTokens(labels_on_page)
+    ex_tokens.collect_tokens(old_tokens, raise_for_no_match=False)
+    transformed = transform_tokens(affine_warp, ex_tokens.extracted_tokens)
+    return ex_tokens, transformed
+
+
+def run_all_docs(config):
+    new_directory = config.new_engine_folder_name
+    new_ocr_path = f"{new_directory}/all_labels.csv"
+    new_ocr_df = pd.read_csv(new_ocr_path, index_col=0)
+    new_file_names = set(new_ocr_df["file_name"])
+
+    old_directory = config.old_engine_folder_name
+    old_ocr_path = f"{old_directory}/all_labels.csv"
+    old_ocr_df = pd.read_csv(old_ocr_path, index_col=0)
+    old_file_names = set(old_ocr_df["file_name"].values)
+
+    common_file_names = old_file_names.intersection(new_file_names)
+    if len(new_file_names) != len(common_file_names):
+        print(
+            f"The following files are present in the old ocr, but not in the new: {','.join(list(new_file_names - common_file_names))}."
+        )
+
+    mappings_by_file_name = {}
+    for f in common_file_names:
+        new_ocr_for_file = new_ocr_df[new_ocr_df["file_name"] == f]
+        old_ocr_for_file = old_ocr_df[old_ocr_df["file_name"] == f]
+        matched_labels_for_doc = run_all_pages_for_doc(
+            f,
+            new_ocr_for_file,
+            old_ocr_for_file,
+            config,
+            debug=False,
+        )
+        mappings_by_file_name[f] = matched_labels_for_doc
+
+    return mappings_by_file_name
+
+
+def run(config, out_file="./summary.xlsx"):
+    with open(config, "r") as file:
+        config_dict = yaml.load(file, Loader=yaml.FullLoader)
+    aligner_config = AlignerConfig(config_dict)
+    all_results = run_all_docs(aligner_config)
+    new_ocr = pd.read_csv(
+        f"./{aligner_config.new_engine_folder_name}/all_labels.csv", index_col=0
+    )
+
+    import pickle
+
+    with open(f"{config.split('.')[0]}.p", "wb") as f:
+        pickle.dump(all_results, f)
+
+    summary_by_file = summarize_results(all_results, new_ocr)
+    convert_to_excel(summary_by_file, out_file)
+
+
+if __name__ == "__main__":
+    Fire(run)
