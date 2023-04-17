cfef4a89578cda5184a2546afc891ac34fe24dd9
Scott <scott.levin@indico.io>
2023-04-17 12:58:18 -0400

    cleaned up instructions and made a few small tweaks to scripts

--- apply_labels.py
+++ apply_labels.py
@@ -1,6 +1,10 @@
 from indico import IndicoClient, IndicoConfig
-from indico.queries import CreateWorkflow, GetWorkflow, AddModelGroupComponent, GetDataset
-from indico.queries.storage import UploadBatched, CreateStorageURLs
+from indico.queries import (
+    CreateWorkflow,
+    GetWorkflow,
+    AddModelGroupComponent,
+    GetDataset,
+)
 from indico.types import NewLabelsetArguments, ModelTaskType
 import os
 import pandas as pd
@@ -8,13 +12,16 @@ import fire
 import json
 import logging
 import tqdm
+import time
 
-from indico.client import GraphQLRequest
+from enum import Enum
 
-class GraphQLMagic(GraphQLRequest):
+from get_datasets import GraphQLMagic
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(query=self.query, variables=kwargs)
+
+class ModelTaskType(Enum):
+    """A list of valid task types for a model group."""
+    ANNOTATION = 6
 
 
 class GetLabelsetMeta(GraphQLMagic):
@@ -33,6 +40,7 @@ class GetLabelsetMeta(GraphQLMagic):
     }
     """
 
+
 class GetQuestionnaireMeta(GraphQLMagic):
     query = """
     query GetQuestionnaireDatafiles($questionnaireId: Int!){
@@ -44,6 +52,7 @@ class GetQuestionnaireMeta(GraphQLMagic):
         }
     }
     """
+
 
 class ModelGroupMeta(GraphQLMagic):
     query = """
@@ -60,6 +69,7 @@ class ModelGroupMeta(GraphQLMagic):
     }
     """
 
+
 class SubmitLabel(GraphQLMagic):
     query = """
     mutation SubmitLabel($labelsetId: Int!, $labels: [LabelInput]!){
@@ -72,14 +82,16 @@ class SubmitLabel(GraphQLMagic):
     }
     """
 
+
 logging.basicConfig(level=os.getenv("LOGGING_LEVEL", "DEBUG"))
+
 
 def get_target_names(revised_labels):
     target_names = set()
     for filename, labels in revised_labels.items():
         for str_page_num, page_labels in labels.items():
             for label in page_labels:
-                target_names.add(label['spans']['label'])
+                target_names.add(label["spans"]["label"])
     return target_names
 
 
@@ -87,26 +99,28 @@ def reformat_labels(label_data_by_page, cls_map):
     reformatted = []
     for str_page_num, page_labels in label_data_by_page.items():
         for label in page_labels:
-            for text_span in label['spans']['text_spans']:
-                text_span['pageNum'] = text_span.pop('page_num')
-                reformatted.append({'clsId': cls_map[label['spans']['label']], 'spans': [text_span]})
+            for text_span in label["spans"]["text_spans"]:
+                text_span["pageNum"] = text_span.pop("page_num")
+                reformatted.append(
+                    {"clsId": cls_map[label["spans"]["label"]], "spans": [text_span]}
+                )
     return reformatted
 
-    
+
 def apply_labels(
-    label_json_path, 
-    new_dataset_id, 
-    new_export_path,
-    workflow_name=None, 
-    workflow_id=None, 
-    mg_name=None, 
-    mg_id=None, 
+    new_dataset_id,
+    label_json_path="./new/revised_labels.json",
+    new_export_path="./new/raw_export.csv",
+    workflow_name=None,
+    workflow_id=None,
+    mg_name=None,
+    mg_id=None,
     datacolumn_name="document",
-    host="app.indico.io", 
-    api_token_path="prod_api_token.txt", 
+    host="app.indico.io",
+    api_token_path="prod_api_token.txt",
 ):
     new_df = pd.read_csv(new_export_path)
-    name_to_file_id = {row['file_name']: row['file_id'] for _, row in new_df.iterrows()}
+    name_to_file_id = {row["file_name"]: row["file_id"] for _, row in new_df.iterrows()}
     client = IndicoClient(config=IndicoConfig(host=host, api_token_path=api_token_path))
     dataset = client.call(GetDataset(id=new_dataset_id))
     revised_labels = json.load(open(label_json_path))
@@ -115,20 +129,21 @@ def apply_labels(
 
     if not workflow_name and not workflow_id:
         raise ValueError("Must provide either workflow_name or workflow_id")
-    
+
     if not mg_name and not mg_id:
         raise ValueError("Must provide either mg_name or mg_id")
 
-
     if workflow_name:
         logging.info("Creating workflow...")
-        workflow = client.call(CreateWorkflow(name=workflow_name, dataset_id=new_dataset_id))
+        workflow = client.call(
+            CreateWorkflow(name=workflow_name, dataset_id=new_dataset_id)
+        )
     else:
         workflow = client.call(GetWorkflow(workflow_id=workflow_id))
 
-    # TODO: pass as arg        
+    # TODO: pass as arg
     after_component_id = workflow.component_by_type("INPUT_OCR_EXTRACTION").id
-    
+
     if mg_name:
         logging.info("Creating model group component...")
         workflow = client.call(
@@ -138,46 +153,58 @@ def apply_labels(
                 after_component_id=after_component_id,
                 source_column_id=dataset.datacolumn_by_name(datacolumn_name).id,
                 new_labelset_args=NewLabelsetArguments(
-                    name=mg_name, 
-                    task_type=ModelTaskType.ANNOTATION, 
+                    name=mg_name,
+                    task_type=ModelTaskType.ANNOTATION,
                     datacolumn_id=dataset.datacolumn_by_name(datacolumn_name).id,
-                    target_names=list(set(target_names))
+                    target_names=list(set(target_names)),
                 ),
                 workflow_id=workflow.id,
             )
         )
-        # Not sure if this is necessary
-        import time; time.sleep(60)
+        time.sleep(60)
 
     matching_component = next(
-        c for c in workflow.components if c.component_type == "MODEL_GROUP"
+        c
+        for c in workflow.components
+        if c.component_type == "MODEL_GROUP"
         and (c.model_group.name == mg_name or c.model_group.id == mg_id)
     )
     model_group_id = matching_component.model_group.id
     questionnaire_id = matching_component.model_group.questionnaire_id
-    model_group_meta = client.call(ModelGroupMeta(modelGroupId=model_group_id))['modelGroup']
-    labelset_id = model_group_meta['labelsetColumnId']
-    examples = model_group_meta['pagedExamples']['examples'] 
+    model_group_meta = client.call(ModelGroupMeta(modelGroupId=model_group_id))[
+        "modelGroup"
+    ]
+    labelset_id = model_group_meta["labelsetColumnId"]
+    examples = model_group_meta["pagedExamples"]["examples"]
 
-    questionnaire = client.call(GetQuestionnaireMeta(questionnaireId=questionnaire_id))['questionnaire']
+    questionnaire = client.call(GetQuestionnaireMeta(questionnaireId=questionnaire_id))[
+        "questionnaire"
+    ]
     labelset_meta = client.call(GetLabelsetMeta(datasetId=new_dataset_id))
     labelset = next(
-        lset for lset in labelset_meta['dataset']['labelsets'] 
-        if lset['id'] == labelset_id
+        lset
+        for lset in labelset_meta["dataset"]["labelsets"]
+        if lset["id"] == labelset_id
     )
-    examples += questionnaire['examples']
-    cls_map = {
-        tname['name']: tname['id'] for tname in labelset['targetNames']
-    }
+    examples += questionnaire["examples"]
+    cls_map = {tname["name"]: tname["id"] for tname in labelset["targetNames"]}
     file_id_to_example_id = {
-        example['datafileId']: example['id'] for example in examples
+        example["datafileId"]: example["id"] for example in examples
     }
     for filename, label_data in tqdm.tqdm(revised_labels.items()):
         print("Processing", filename)
         file_id = name_to_file_id[filename]
         example_id = file_id_to_example_id[file_id]
         targets = reformat_labels(label_data, cls_map)
-        client.call(SubmitLabel(labelsetId=labelset_id, labels=[{'exampleId': example_id, 'targets': targets, 'override': True}]))
+        client.call(
+            SubmitLabel(
+                labelsetId=labelset_id,
+                labels=[
+                    {"exampleId": example_id, "targets": targets, "override": True}
+                ],
+            )
+        )
+
 
 if __name__ == "__main__":
     fire.Fire(apply_labels)
--- get_datasets.py
+++ get_datasets.py
@@ -19,14 +19,16 @@ from indico.errors import IndicoError
 from indico.queries import (
     RetrieveStorageObject,
     GetDataset,
-    DownloadExport, CreateExport
+    DownloadExport,
+    CreateExport,
 )
 from indico.client import GraphQLRequest
 
-class GraphQLMagic(GraphQLRequest):
 
+class GraphQLMagic(GraphQLRequest):
     def __init__(self, *args, **kwargs):
         super().__init__(query=self.query, variables=kwargs)
+
 
 class GetDatafileIDs(GraphQLMagic):
     query = """
@@ -41,6 +43,8 @@ class GetDatafileIDs(GraphQLMagic):
         }
     }
     """
+
+
 class GetDatafileByID(GraphQLMagic):
     query = """
     query getDatafileById($datafileId: Int!) {
@@ -68,13 +72,13 @@ class GetLabelsetName(GraphQLMagic):
     }
     """
 
+
 def get_export(client, dataset_id, labelset_id=None):
     # Get dataset object
     dataset = client.call(GetDataset(id=dataset_id))
-    
+
     if labelset_id is None and dataset.labelsets:
         labelset_id = dataset.labelsets[0].id
-
 
     if labelset_id is not None:
         # Create export object using dataset's id and labelset id
@@ -87,7 +91,7 @@ def get_export(client, dataset_id, labelset_id=None):
                 wait=True,
             )
         )
-    
+
         # Use export object to download as pandas csv
         logging.info("Downloading export...")
         df = client.call(DownloadExport(export.id))
@@ -97,23 +101,27 @@ def get_export(client, dataset_id, labelset_id=None):
 
     return df
 
+
 def generate_fake_export_sans_labels(client, dataset_id):
     """
     Get text of each doc and convert to a pd.DataFrame
     """
-    datafiles = client.call(GetDatafileIDs(datasetId=dataset_id))['dataset']['files']
+    datafiles = client.call(GetDatafileIDs(datasetId=dataset_id))["dataset"]["files"]
     records = []
     for datafile in datafiles:
-        records.append({
-            'file_id': datafile['id'],
-            'file_name': datafile['name'],
-            'file_url': datafile['rainbowUrl']
-        })
+        records.append(
+            {
+                "file_id": datafile["id"],
+                "file_name": datafile["name"],
+                "file_url": datafile["rainbowUrl"],
+            }
+        )
     return pd.DataFrame.from_records(records)
 
 
 def text_from_ocr(page_ocrs):
-    return "\n".join(page['pages'][0]['text'] for page in page_ocrs)
+    return "\n".join(page["pages"][0]["text"] for page in page_ocrs)
+
 
 def reformat_labels(labels, document):
     spans_labels = json.loads(labels)
@@ -139,45 +147,61 @@ def get_ocr_by_datafile_id(client, datafile_id, dataset_dir, filename):
     """
     datafile_meta = client.call(GetDatafileByID(datafileId=datafile_id))
     page_ocrs, page_images = [], []
-    local_page_image_dir = os.path.join(dataset_dir, 'images', filename)
-    local_page_json_dir = os.path.join(dataset_dir, 'jsons', filename)
+    local_page_image_dir = os.path.join(dataset_dir, "images", filename)
+    local_page_json_dir = os.path.join(dataset_dir, "jsons", filename)
     os.makedirs(local_page_image_dir, exist_ok=True)
     os.makedirs(local_page_json_dir, exist_ok=True)
-    for page in datafile_meta['datafile']['pages']:
-        page_info = page['pageInfo']
-        page_json_file = os.path.join(local_page_json_dir, f"page_{page['pageNum']}.json")
-        page_image_file = os.path.join(local_page_image_dir, f"page_{page['pageNum']}.png")
+    for page in datafile_meta["datafile"]["pages"]:
+        page_info = page["pageInfo"]
+        page_json_file = os.path.join(
+            local_page_json_dir, f"page_{page['pageNum']}.json"
+        )
+        page_image_file = os.path.join(
+            local_page_image_dir, f"page_{page['pageNum']}.png"
+        )
         if os.path.exists(page_json_file):
             page_ocr = json.load(open(page_json_file))
         else:
-            page_ocr = client.call(RetrieveStorageObject(page['pageInfo']))
+            page_ocr = client.call(RetrieveStorageObject(page["pageInfo"]))
         # Could just return page image and save to file in inner loop if required
         if not os.path.exists(page_image_file):
-            page_image = client.call(RetrieveStorageObject(page['image']))
-            with open(page_image_file, 'wb') as fd:
+            page_image = client.call(RetrieveStorageObject(page["image"]))
+            with open(page_image_file, "wb") as fd:
                 fd.write(page_image)
         page_ocrs.append(page_ocr)
         page_images.append(page_image_file)
     return page_ocrs, page_images
 
 
-def get_dataset(name, dataset_id, labelset_id=None, label_col="labels", text_col="pdf", filename_col='file_name', host="app.indico.io", api_token_path="prod_api_token.txt"):
+def get_dataset(
+    name,
+    dataset_id,
+    labelset_id=None,
+    label_col="labels",
+    text_col="document",
+    filename_col="file_name",
+    host="app.indico.io",
+    api_token_path="prod_api_token.txt",
+):
     # TODO: Get label col name from labelset metadata
     os.makedirs(name, exist_ok=True)
     os.makedirs(os.path.join(name, "images"), exist_ok=True)
     os.makedirs(os.path.join(name, "files"), exist_ok=True)
-
     my_config = IndicoConfig(
-        host=host, api_token_path=api_token_path,
+        host=host,
+        api_token_path=api_token_path,
     )
     client = IndicoClient(config=my_config)
     if labelset_id:
         labelset = next(
-            labelset for labelset in client.call(GetLabelsetName(datasetId=dataset_id))['dataset']['labelsets']
-            if labelset['id'] == labelset_id
+            labelset
+            for labelset in client.call(GetLabelsetName(datasetId=dataset_id))[
+                "dataset"
+            ]["labelsets"]
+            if labelset["id"] == labelset_id
         )
-        label_col = labelset['name']
-        
+        label_col = labelset["name"]
+
     export_path = os.path.join(name, "raw_export.csv")
 
     if not os.path.exists(export_path):
@@ -191,26 +215,26 @@ def get_dataset(name, dataset_id, labelset_id=None, label_col="labels", text_col
 
     for i, row in enumerate(tqdm.tqdm(records)):
         filename = os.path.splitext(os.path.basename(row[filename_col]))[0]
-        document_path = os.path.join(name, "files", filename + "." + row["file_name"].split(".")[-1])
-        page_ocrs, page_image_paths = get_ocr_by_datafile_id(client, row['file_id'], dataset_dir=name, filename=filename)
+        document_path = os.path.join(
+            name, "files", filename + "." + row["file_name"].split(".")[-1]
+        )
+        page_ocrs, page_image_paths = get_ocr_by_datafile_id(
+            client, row["file_id"], dataset_dir=name, filename=filename
+        )
 
         # Try to get text from export, but fallback to reconstructing from page OCR
         if text_col in row:
             text = row[text_col]
         else:
             text = text_from_ocr(page_ocrs)
-        
+
         # DF doesn't have labels or labels are null for a file
         if label_col not in row or pd.isna(row[label_col]):
             labels = None
         else:
             labels = reformat_labels(row[label_col], text)
-        
-        output_record = {
-            "ocr": json.dumps(page_ocrs),
-            "text": text,
-            "labels": labels
-        }
+
+        output_record = {"ocr": json.dumps(page_ocrs), "text": text, "labels": labels}
         output_record["image_files"] = json.dumps(page_image_paths)
         output_record["document_path"] = document_path
 
@@ -222,7 +246,7 @@ def get_dataset(name, dataset_id, labelset_id=None, label_col="labels", text_col
     csv_path = os.path.join(name, "all_labels.csv")
     logger.info("Creating CSV...")
     pd.DataFrame.from_records(output_records).to_csv(csv_path, index=False)
-        
-    
+
+
 if __name__ == "__main__":
     fire.Fire(get_dataset)
